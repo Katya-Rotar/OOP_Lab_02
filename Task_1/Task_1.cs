8fc7f8ab99cba3f75c3a9a7514e93bfa43fe3e56
user <katya.rotar02@gmail.com>
2023-09-10 10:47:31 +0300

    Add project files.

---
+++ Task_1/Task_1.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+// Прочитати два масиви слів і знайдіть довжину найбільшого спільного початку або кінця 
+string input_1 = Console.ReadLine();
+string input_2 = Console.ReadLine();
+
+string[] array_1, array_2;
+if (input_1.Length < input_2.Length)
+{
+    array_1 = input_1.Split(' ');
+    array_2 = input_2.Split(' ');
+}
+else {
+    array_1 = input_2.Split(' ');
+    array_2 = input_1.Split(' ');
+}
+int output = 0;
+for (int i = 0; i < 2; i++)
+{
+    output = 0;
+    for (int j = 0; j < array_1.Length; j++)
+    {
+        if (array_1[j].Equals(array_2[j])){
+            output++;
+        }
+        else
+        {
+            Array.Reverse(array_1);
+            Array.Reverse(array_2);
+            break;
+        }
+    } 
+}
+Console.WriteLine(output);
---
+++ Task_2/Task_2.cs
@@ -0,0 +1,27 @@
+﻿// Сума обертань масиву
+
+string[] input = Console.ReadLine().Split(' ');
+int[] array_input= Array.ConvertAll(input, int.Parse);
+
+//int n = Convert.ToInt32(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+int[] sum = new int[array_input.Length];
+for (int i = 0; i < n; i++)
+{
+    int[] rotated = new int[array_input.Length];
+    rotated[0] = array_input[array_input.Length - 1];
+    for (int j = 1; j < array_input.Length; j++) {
+        rotated[j] = array_input[j - 1];
+    }
+
+    for (int j = 0; j < array_input.Length; j++) {
+        sum[j] += rotated[j];
+        array_input[j] = rotated[j];
+        if (i == n - 1)
+        {
+            Console.Write(sum[j] + " ");
+        }
+    }
+
+}

09cddce4ddba942855fd93f3119a69e2fef2a84f
user <katya.rotar02@gmail.com>
2023-09-11 18:48:14 +0300

    Task_6 and Task_7 solution

---
+++ Task_6/Task_6.cs
@@ -0,0 +1,26 @@
+﻿int[] array_input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+int start = 0, len = 1;
+int bestStart = 0, bestLen = 0;
+for (int i = 1; i < array_input.Length; i++) {
+    if (array_input[i] == array_input[start])
+    {
+        len++;
+    }
+    else {
+        if(len > bestLen)
+        {
+            bestLen = len;
+            bestStart = start;
+        }
+        start = i;
+        len = 1;
+    }
+    if (start + len == array_input.Length && len > bestLen)
+    {
+        bestLen = len;
+        bestStart = start;
+    }
+}
+for (int i = bestStart; i < bestStart + bestLen; i++) {
+    Console.Write(array_input[i] + " ");
+}
---
+++ Task_7/Task_7.cs
@@ -0,0 +1,31 @@
+﻿int[] array_input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+int start = 0, len = 1, n = 1;
+int bestStart = 0, bestLen = 0;
+for (int i = 1; i < array_input.Length; i++)
+{
+    if (array_input[i] == array_input[start] + n)
+    {
+        len++;
+        n++;
+    }
+    else
+    {
+        if (len > bestLen)
+        {
+            bestLen = len;
+            bestStart = start;
+        }
+        start = i;
+        len = 1;
+        n = 1;
+    }
+    if (start + len == array_input.Length && len > bestLen)
+    {
+        bestLen = len;
+        bestStart = start;
+    }
+}
+for (int i = bestStart; i < bestStart + bestLen; i++)
+{
+    Console.Write(array_input[i] + " ");
+}
